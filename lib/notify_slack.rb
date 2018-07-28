96c7abd1a8ce065c1ec5b138b9ee9d30341496b4
Patrick J. Sparrow <psparrow25@gmail.com>
2018-07-28 13:04:56 -0400

    Dependency injection

--- lib/notify_slack.rb
+++ lib/notify_slack.rb
@@ -5,10 +5,9 @@ class NotifySlack
     required(:message).filled
   end
 
-  def self.call(message:)    
+  def self.call(message:, notifier: Slack::Notifier.new(SLACK_WEBHOOK_URL))    
     return false unless Schema.call(message: message).success?
-    
-    notifier = Slack::Notifier.new(SLACK_WEBHOOK_URL)  
+        
     notifier.ping(message)
   end
 end
