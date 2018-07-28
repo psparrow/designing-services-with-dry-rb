cd1b4fef03f152893432e50335ce7867a68339d6
Patrick J. Sparrow <psparrow25@gmail.com>
2018-07-28 13:27:34 -0400

    Introduce dry-initializer

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'dry-initializer'
 gem 'dry-validation'
 gem 'pry'
--- lib/notify_slack.rb
+++ lib/notify_slack.rb
@@ -1,13 +1,25 @@
+require 'dry-initializer'
 require 'dry-validation'
+
+URL = 'foo'
 
 class NotifySlack
   Schema = Dry::Validation.Schema do
     required(:message).filled
   end
 
-  def self.call(message:, notifier: Slack::Notifier.new(SLACK_WEBHOOK_URL))    
-    return false unless Schema.call(message: message).success?
-        
+  extend Dry::Initializer
+
+  option :message
+  option :notifier, default: -> { Slack::Notifier.new(URL) }
+  
+  def self.call(**args)
+    return false unless Schema.call(args).success?
+    
+    new(**args).call
+  end
+
+  def call
     notifier.ping(message)
   end
 end
